a1184bac84b302ce169ac05a11d28652291bb561
Nole35 <lazic.sr9.2020@uns.ac.rs>
2023-04-26 14:34:55 +0200

    kommit

---
+++ app/src/main/java/com/example/slagalicakviz/KoZnaZna.java
@@ -0,0 +1,14 @@
+package com.example.slagalicakviz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KoZnaZna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ko_zna_zna);
+    }
+}
---
+++ app/src/main/java/com/example/slagalicakviz/Spojnice.java
@@ -0,0 +1,14 @@
+package com.example.slagalicakviz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Spojnice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spojnice);
+    }
+}
