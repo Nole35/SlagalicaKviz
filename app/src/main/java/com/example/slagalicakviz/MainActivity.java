0a0125027e36d9d0c981959d79d3d270ba947123
Kristina T <kikitiki1989@gmail.com>
2023-04-28 01:01:23 +0200

    feat:dodavanje login i registracije activity

--- app/src/main/java/com/example/slagalicakviz/MainActivity.java
+++ app/src/main/java/com/example/slagalicakviz/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.slagalicakviz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button =findViewById(R.id.loginbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button1 = findViewById(R.id.registracijaBtn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegistracijaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/slagalicakviz/RegistracijaActivity.java
@@ -0,0 +1,14 @@
+package com.example.slagalicakviz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistracijaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registracija);
+    }
+}
